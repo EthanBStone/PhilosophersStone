9db5f54644710f06a7ec93ba541688b02f0ffbab
EthanBStone <72464126+EthanBStone@users.noreply.github.com>
2021-05-31 16:36:56 -0700

    Released on workshop

--- main.lua
+++ main.lua
@@ -9,9 +9,7 @@ local GameState = {}
 PStone.COLLECTIBLE_P_STONE = Isaac.GetItemIdByName("The Philosopher's Stone")
 
 --[[
-*Add troll bombs support
 *Add poop support
-*Add chests support
 *Fix it with void, void instantly uses it unlike other 1 use items
 
 
@@ -26,12 +24,39 @@ PStone.pStoneCap = {
 	3, 		--Cap for pills, 3 should be ok since you might not want to fully use them anyway
 	1,		--Cap for batteries, should be 1 since the battery moves to a different room and hurts you, higher than 1 could 
 			--accidentally make softlocks and force you to take dmg
-	TRINKETS = 999,	--Cap for trinkets, I made it infinite since you can only hold 1 trinket anyway
+	CHESTS = 4,
+	TRINKETS = 50,	--Cap for trinkets, I made it nearly infinite since you can only hold 1 trinket anyway
 
 	ENEMIES = 10,	--Cap for enemies
-
-
+	TROLLBOMBS = 4,
 }
+--Convert certain item pedestal into their golden variant
+PStone.ItemConversions = {
+	{Target = CollectibleType.COLLECTIBLE_IRON_BAR, 
+	ConvertTo = CollectibleType.COLLECTIBLE_MIDAS_TOUCH}, --iron bar to midas touch
+
+	{Target = CollectibleType.COLLECTIBLE_RAZOR_BLADE, 
+	ConvertTo = CollectibleType.COLLECTIBLE_GOLDEN_RAZOR}, --razor to golden razor
+	
+	{Target = CollectibleType.COLLECTIBLE_TELEPORT, 
+	ConvertTo = CollectibleType.COLLECTIBLE_TELEPORT_2}, --teleport to teleport 2.0
+
+	{Target = CollectibleType.COLLECTIBLE_CIRCLE_OF_PROTECTION, 
+	ConvertTo = CollectibleType.COLLECTIBLE_DADS_RING}, --circle of protection to dads ring
+	
+	{Target = CollectibleType.COLLECTIBLE_BROTHER_BOBBY, 
+	ConvertTo = CollectibleType.COLLECTIBLE_KING_BABY}, --brother bobby to king baby
+
+	{Target = CollectibleType.COLLECTIBLE_LATCH_KEY, 
+	ConvertTo = CollectibleType.COLLECTIBLE_MOMS_KEY}, --latch key to moms key
+
+	{Target = CollectibleType.COLLECTIBLE_THERES_OPTIONS, 
+	ConvertTo = CollectibleType.COLLECTIBLE_MORE_OPTIONS}, --Theres options to more options
+
+	{Target = CollectibleType.COLLECTIBLE_OPTIONS, 
+	ConvertTo = CollectibleType.COLLECTIBLE_MORE_OPTIONS}, --options? to more options				
+}
+
 function PStone:onUpdate()
 	--print("Num trinkets = " .. TrinketType.NUM_TRINKETS)
 	if game:GetFrameCount()  == 5 then
@@ -39,6 +64,7 @@ function PStone:onUpdate()
 		Isaac.ExecuteCommand("debug 8")
 		player:AddCard(7) --Lovers card
 		--player:UseCard(10) --Use hermit
+		player:AddCollectible(534) --Schoolbag
 		Isaac.Spawn(EntityType.ENTITY_PICKUP, 100, 439, Vector(380,300), Vector(0,0), nil) -- Moms box
 		for cnt = 1, 3 do
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, 40, 1, Vector(0,0), Vector(0,0), nil) -- Bomb
@@ -46,7 +72,9 @@ function PStone:onUpdate()
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, 1, Vector(300,0), Vector(0,0), nil) -- Battery
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_PILL, 1, Vector(450,0), Vector(0,0), nil) -- Pills
 		end
-
+		Isaac.Spawn(EntityType.ENTITY_PICKUP, 100, CollectibleType.COLLECTIBLE_IRON_BAR, Vector(400,330), Vector(0,0), nil) -- Iron bar
+		
+		player:AddCollectible(65, 12) --Anarchist cookbook
 	end
 	if game:GetFrameCount() % 30 == 1 then
 		player = game:GetPlayer(0)
@@ -73,22 +101,48 @@ function PStone:onPStoneUse(item, rmg, player, useFlags, slot, customData)
 		{Count = 0, Variant = PickupVariant.PICKUP_LIL_BATTERY, SubType = BatterySubType.BATTERY_GOLDEN},
 	}	
 
+	--Set to 1 if no car battery, otherwise 2. This is the cap multiplier
+	local carBatteryMult = 1
+	if player:HasCollectible(CollectibleType.COLLECTIBLE_CAR_BATTERY) then
+		carBatteryMult = 2
+	end
 	local trinketsConverted = 0
+	local chestsConverted = 0
+	local trollBombsConverted = 0
 	--Convert Pickups
 	for _, pickup in pairs(Isaac.FindByType(EntityType.ENTITY_PICKUP)) do
 		variant = pickup.Variant
 		subtype = pickup.SubType
 		pickup = pickup:ToPickup()
-		
-		if variant == PickupVariant.PICKUP_TRINKET and subtype < 32768 and trinketsConverted < PStone.pStoneCap.TRINKETS then
+		--Check to see if its a chest
+		local isChest = (variant == PickupVariant.PICKUP_CHEST or variant == PickupVariant.PICKUP_BOMBCHEST or variant == PickupVariant.PICKUP_SPIKEDCHEST or variant == PickupVariant.PICKUP_MIMICCHEST or variant == PickupVariant.PICKUP_OLDCHEST or variant == PickupVariant.PICKUP_WOODENCHEST or variant == PickupVariant.PICKUP_HAUNTEDCHEST)
+	
+		--Trinket conversion
+		if variant == PickupVariant.PICKUP_TRINKET and subtype < 32768 and trinketsConverted < PStone.pStoneCap.TRINKETS * carBatteryMult then
 			trinketsConverted = trinketsConverted + 1
 			pickup:Morph(EntityType.ENTITY_PICKUP, variant, subtype + 32768, true, true)
+		--Chest conversion
+		elseif isChest and subtype == ChestSubType.CHEST_CLOSED and chestsConverted < PStone.pStoneCap.CHESTS * carBatteryMult then
+			chestsConverted = chestsConverted + 1
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LOCKEDCHEST, subtype, true, true)
+				
 		else
 			for i = 1, 6 do
-				if variant == convertedData[i].Variant and subtype ~= convertedData[i].SubType and  convertedData[i].Count < PStone.pStoneCap[i] then
+				--For item conversions
+				if variant == PickupVariant.PICKUP_COLLECTIBLE then
+					for ind, items in pairs(PStone.ItemConversions) do
+						if subtype == items.Target then
+							--print("Converting " .. subtype .. " To " .. items.ConvertTo)
+							pickup:Morph(EntityType.ENTITY_PICKUP, variant, items.ConvertTo, true, true)
+						end
+						--print("Subtype = " .. subtype)
+					end
+				
+				elseif variant == convertedData[i].Variant and subtype ~= convertedData[i].SubType and  convertedData[i].Count < (PStone.pStoneCap[i] * carBatteryMult) then
 					convertedData[i].Count = convertedData[i].Count + 1
 					pickup:Morph(EntityType.ENTITY_PICKUP, variant, convertedData[i].SubType, true, true)
-				end		
+				end
+				
 				
 			end
 		end	
@@ -97,10 +151,19 @@ function PStone:onPStoneUse(item, rmg, player, useFlags, slot, customData)
 	local enemiesConverted = 0
 	--Convert Enemies
 	for _, enemy in pairs(Isaac.FindInRadius(game:GetRoom():GetCenterPos(), 1500, EntityPartition.ENEMY)) do
-		enemy = enemy:ToNPC()
-		if enemiesConverted < PStone.pStoneCap.ENEMIES and not enemy:IsBoss() then
-			enemy:AddMidasFreeze(EntityRef(player), 8 * 30)
+		if enemy:ToNPC() ~= nil then
+			enemy = enemy:ToNPC()
+			if enemiesConverted < (PStone.pStoneCap.ENEMIES * carBatteryMult) and not enemy:IsBoss() then
+				enemy:AddMidasFreeze(EntityRef(player), 8 * 30)
+			end
+		--Check for trollbomb
+		elseif enemy.Type == EntityType.ENTITY_BOMBDROP and (enemy.Variant == BombVariant.BOMB_TROLL or enemy.Variant == BombVariant.BOMB_SUPERTROLL) and trollBombsConverted < PStone.pStoneCap.TROLLBOMBS  then
+			trollBombsConverted = trollBombsConverted + 1
+			enemy:Kill()
+			Isaac.Spawn(enemy.Type, BombVariant.BOMB_GOLDENTROLL, enemy.SubType, enemy.Position, enemy.Velocity, nil)
 		end
+		
+
 		
 	
 	end
@@ -114,85 +177,10 @@ function PStone:onPStoneUse(item, rmg, player, useFlags, slot, customData)
 	
 	return infoTable
 end
---Old function that works
---[[
-function PStone:onPStoneUse(item, rmg, player, useFlags, slot, customData)
-	--print("Philosopher's Stone used!")
-	--print("Use flags: " .. useFlags)
-
-
-	local convertedCount = {
-		0, --heartsConverted
-		0, --coinsConverted
-		0, --keysConverted
-		0, --bombsConverted
-		0, --pillsConverted
-		0, --batteryConverted
-	}	
-	local convertedVariant = {
-		PickupVariant.PICKUP_HEART,
-		PickupVariant.PICKUP_COIN,
-		PickupVariant.PICKUP_KEY,
-		PickupVariant.PICKUP_BOMB,
-		PickupVariant.PICKUP_PILL,
-		PickupVariant.PICKUP_LIL_BATTERY,
-	}
-	
-	local goldSubtype = {
-		HeartSubType.HEART_GOLDEN,
-		CoinSubType.COIN_GOLDEN,
-		KeySubType.KEY_GOLDEN,
-		BombSubType.BOMB_GOLDEN,
-		PillColor.PILL_GOLD,
-		BatterySubType.BATTERY_GOLDEN,	
-	}
-	
-	local trinketsConverted = 0
-	--Convert Pickups
-	for _, pickup in pairs(Isaac.FindByType(EntityType.ENTITY_PICKUP)) do
-		variant = pickup.Variant
-		subtype = pickup.SubType
-		pickup = pickup:ToPickup()
-		
-		if variant == PickupVariant.PICKUP_TRINKET and subtype < 32768 and trinketsConverted < PStone.pStoneCap.TRINKETS then
-			trinketsConverted = trinketsConverted + 1
-			pickup:Morph(EntityType.ENTITY_PICKUP, variant, subtype + 32768, true, true)
-		else
-			for i = 1, 6 do
-				if variant == convertedVariant[i] and subtype ~= goldSubtype[i] and  convertedCount[i] < PStone.pStoneCap[i] then
-					convertedCount[i] = convertedCount[i] + 1
-					pickup:Morph(EntityType.ENTITY_PICKUP, variant, goldSubtype[i], true, true)
-				end		
-				
-			end
-		end	
-	end
-	
-	local enemiesConverted = 0
-	--Convert Enemies
-	for _, enemy in pairs(Isaac.FindInRadius(game:GetRoom():GetCenterPos(), 1500, EntityPartition.ENEMY)) do
-		enemy = enemy:ToNPC()
-		if enemiesConverted < PStone.pStoneCap.ENEMIES and not enemy:IsBoss() then
-			enemy:AddMidasFreeze(EntityRef(player), 8 * 30)
-		end
-		
-	
-	end
-	
-	--Item is 1 use
-	local infoTable = {
-		Remove = true,
-		ShowAnim = true,
-	
-	}	
-	
-	return infoTable
-end
-]]--
 
 PStone:AddCallback(ModCallbacks.MC_POST_UPDATE, PStone.onUpdate)
 PStone:AddCallback(ModCallbacks.MC_USE_ITEM, PStone.onPStoneUse, PStone.COLLECTIBLE_P_STONE)
 
 if EID then
-	EID:addCollectible(PStone.COLLECTIBLE_P_STONE, "Turns some coins, bombs, keys, pills, hearts, batteries, and trikets on the ground into their golden version.#Temporarily turns some non-boss enemies in the room into golden statues.")
+	EID:addCollectible(PStone.COLLECTIBLE_P_STONE, "#Single use.#Turns some pickups(ie keys, trinkets, ect) on the ground into their golden version.#Temporarily turns some non-boss enemies in the room into golden statues.#Also converts some item pedestals like Teleport into their golden version")
 end
